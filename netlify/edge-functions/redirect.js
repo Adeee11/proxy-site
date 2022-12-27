f5c6ce8fb7e36e0a7267cb02f37e0e33184816ca
Adeee11 <hello@iwebcode.design>
2022-12-27 15:57:37 +0530

    redirect

--- netlify/edge-functions/redirect.js
+++ netlify/edge-functions/redirect.js
@@ -7,12 +7,12 @@ export default async (request, context) => {
         status: 302,
         headers
     }
-    // return new Response("do it!", init)
-    const response = await context.next();
-    const page = await response.text();
-    const regex = /Get Started/i;
-    const msg = `Hiii`
-    const updatedPage = page.replace(regex, msg);
+    return new Response("do it!", init)
+    // const response = await context.next();
+    // const page = await response.text();
+    // const regex = /Get Started/i;
+    // const msg = `Hiii`
+    // const updatedPage = page.replace(regex, msg);
 
-    return new Response(updatedPage, response)
+    // return new Response(updatedPage, response)
 }
