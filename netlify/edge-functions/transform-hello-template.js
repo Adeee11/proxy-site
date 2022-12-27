9160880c407ffd09ef326289c65e98e3d88b39cf
Adeee11 <hello@iwebcode.design>
2022-12-27 15:37:12 +0530

    redirect

--- netlify/edge-functions/transform-hello-template.js
+++ netlify/edge-functions/transform-hello-template.js
@@ -1,10 +1,6 @@
 export default async (request, context) => {
     const url = new URL(request.url)
 
-    if (url.searchParams.get("method") !== "transform") {
-        return;
-    }
-
     const response = await context.next();
     const page = await response.text();
     const regex = /LOCATION_UNKNOWN/i;
