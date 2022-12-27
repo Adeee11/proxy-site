556f0d7a43cababeb80e89828d71ee90131b3f55
eram-iw <eram.perween@iwebcode.dev>
2022-12-27 12:21:21 +0530

    first commit

---
+++ .cache/page-ssr/routes/component---src-pages-index-tsx.js
@@ -0,0 +1,28 @@
+"use strict";
+exports.id = "component---src-pages-index-tsx";
+exports.ids = ["component---src-pages-index-tsx"];
+exports.modules = {
+
+/***/ "./src/pages/index.tsx?export=default":
+/*!********************************************!*\
+  !*** ./src/pages/index.tsx?export=default ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+const HomePage = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h1", null, "Hello World!!!"));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-index-tsx.js.map
