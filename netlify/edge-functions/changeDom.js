7cb284f3cc85e3b9acf6695b1ae4afb357d6b54d
Adeee11 <hello@iwebcode.design>
2022-12-29 10:44:36 +0530

    test

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,20 +1,10 @@
 export default async (request, context) => {
-
-    // const headers = {
-    //     Location: 'https://www.google.com/'
-    // }
-    // const init = {
-    //     status: 302,
-    //     headers
-    // }
-    // return new Response("successful!", init)
     const response = await context.next();
     const page = await response.text();
     const regex = /Get Started/i;
     const msg = `Hiii`
     const updatedPage = page.replace(regex, msg);
 
-    // return new Response(updatedPage, response)
-    return new Response("We did it!!!")
+    return new Response("helllo")
 }
 
