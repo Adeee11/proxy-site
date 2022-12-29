4b174b7164316afd1779d8f615891ccb71b1d676
Adeee11 <hello@iwebcode.design>
2022-12-29 11:03:26 +0530

    changes

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,27 +1,10 @@
-// export default async (request, context) => {
-//     const response = await context.next();
-//     const page = await response.text();
-//     const regex = /Get Started/i;
-//     const msg = `Hiii`
-//     const updatedPage = page.replace(regex, msg);
-
-//     return new Response("helllo")
-// }
-
-
-
 export default async (request, context) => {
-    let promise = new Promise(async (resolve, reject) => {
-        return new Response.redirect("https://www.testhq.com/", 302)
-    });
-    let result = await promise; // wait until the promise resolves (*)
-    if (result) {
-        const response = await context.next();
-        const page = await response.text();
-        const regex = /Get Started/i;
-        const msg = `Hiii`
-        const updatedPage = page.replace(regex, msg);
+    // const response = await context.next();
+    // const page = await response.text();
+    // const regex = /Get Started/i;
+    // const msg = `Hiii`
+    // const updatedPage = page.replace(regex, msg);
 
-        return new Response("helllo")
-    }
+    return new Response("helllo")
 }
+
--- netlify/edge-functions/redirect.js
+++ netlify/edge-functions/redirect.js
@@ -1,24 +1,6 @@
 
-// export default () => {
-//     return new Response.redirect("https://www.testhq.com/", 302)
-// }
-
-
-
-export default async (request, context) => {
-    let promise = new Promise(async (resolve, reject) => {
-        return new Response.redirect("https://www.testhq.com/", 302)
-    });
-    let result = await promise; // wait until the promise resolves (*)
-    if (result) {
-        const response = await context.next();
-        const page = await response.text();
-        const regex = /Get Started/i;
-        const msg = `Hiii`
-        const updatedPage = page.replace(regex, msg);
-
-        return new Response("helllo")
-    }
+export default () => {
+    return new Response.redirect("https://www.testhq.com/", 302)
 }
 
 
