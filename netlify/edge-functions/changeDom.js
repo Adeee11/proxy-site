2ffa51917b9ed35cd0dc8a5b0fa9de48b2047b0e
Adeee11 <hello@iwebcode.design>
2023-01-03 14:24:02 +0530

    tetsing

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,10 +1,9 @@
 export default async (request, context) => {
-    const url = 'https://www.testhq.com/'
     const response = await context.next();
-    const page = await response.data.text();
-    const regex = 'Get Started';
-    const msg = 'Hello World'
-    const updatedPage = page.replaceAll(regex, msg);
+    const page = await response.text();
+    const regex = 'Hello';
+    const msg = 'Hiii'
+    const updatedPage = page.replace(regex, msg);
 
-    return new Response(updatedPage, response)
+    return new Response(updatedPage)
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,6 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const Home = () => {
-    // var requestOptions = {
-    //     method: 'GET',
-    // };
-
-    // fetch("https://www.testhq.com/", requestOptions)
-    //     .then(response => response.text())
-    //     .then(result => console.log('result', result))
-    //     .catch(error => console.log('error', error));
-
     return (
         <div>
             <h1>Hello World!!!!</h1>
