a33a43447a831b0f4f4d6ec305892efa78390ad2
Adeee11 <hello@iwebcode.design>
2023-01-03 14:58:30 +0530

    changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "__cf_bm=37qfEfn4CpMbhmwK7236vGMse2ESLMqXkM4kWTPiE4g-1672737564-0-Afp9WEPgDMFFgHpPLsL/ibHJuKGEXxQLv6c0clK+dXIHHvsXr51rglI21qLXa7glc7xkEBIdXgkSzqymLA9mPFE=; __cfruid=501746bb133d6ac0c18eb790f097f6afd7bd2755-1672737564");
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    } as any;
+    useEffect(() => {
+        fetch("https://www.testhq.com/", requestOptions)
+            .then(response => response)
+            .then(result => console.log('hello', result))
+            .catch(error => console.log('error', error));
+    }, [])
+
     return (
         <div>
             <h1>Hello World!!!!</h1>
