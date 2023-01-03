d06f57da748c679cede0309b77e7f09531248211
Adeee11 <hello@iwebcode.design>
2023-01-03 18:35:02 +0530

    changes

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -2,8 +2,8 @@ export default async (request, context) => {
     const response = await context.next();
     const page = await response.text();
     const regex = 'Get Started';
-    const msg = 'Hiii'
-    const updatedPage = page.replace(regex, msg);
+    const msg = 'Changed Content'
+    const updatedPage = page.replaceAll(regex, msg);
 
     return new Response(updatedPage, response)
 }
