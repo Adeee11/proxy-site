cd9b88105c2fb244c6000c3873bf717c4edb3a8e
Adeee11 <hello@iwebcode.design>
2023-01-03 17:26:46 +0530

    changes

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,20 +1,9 @@
 export default async (request, context) => {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+    const response = await context.next();
+    const page = await response.text();
+    const regex = 'Hello';
+    const msg = 'Hiii'
+    const updatedPage = page.replace(regex, msg);
 
-    fetch("https://04d4-122-176-42-49.in.ngrok.io/modify", requestOptions)
-        .then(response => response.text())
-        .then(result => {
-            return new Response(result, response)
-        })
-        .catch(error => console.log('error', error));
-    // const response = await context.next();
-    // const page = await response.text();
-    // const regex = 'Hello';
-    // const msg = 'Hiii'
-    // const updatedPage = page.replace(regex, msg);
-
-    // return new Response(updatedPage, response)
+    return new Response(updatedPage, response)
 }
