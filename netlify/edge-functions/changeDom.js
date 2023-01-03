80d34c96dba61a71597e0d5503c0907c2df34f42
Adeee11 <hello@iwebcode.design>
2023-01-03 17:22:31 +0530

    test

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,31 +1,15 @@
-import axios from "axios";
-
 export default async (request, context) => {
-    var config = {
-        method: 'get',
-        url: 'https://04d4-122-176-42-49.in.ngrok.io/modify',
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Vary": "Access-Control-Request-Headers",
-            "Access-Control-Allow-Headers": "Content-Type,Accept"
-        }
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
     };
-    const response = await context.next();
-    const page = await response.text();
 
-
-    axios(config)
-        .then(function (resp) {
-            // const updatedPage = page.replace(regex, msg);
-
-            return new Response(resp, response)
-
-            // console.log(response);
+    fetch("https://04d4-122-176-42-49.in.ngrok.io/modify", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            return new Response(result, response)
         })
-        .catch(function (error) {
-            console.log(error);
-        });
+        .catch(error => console.log('error', error));
     // const response = await context.next();
     // const page = await response.text();
     // const regex = 'Hello';
