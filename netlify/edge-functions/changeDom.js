739c48bdcbd368092a5d1238df6d97a95052e7bf
Adeee11 <hello@iwebcode.design>
2023-01-03 12:47:52 +0530

    testing

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,9 +1,11 @@
 export default async (request, context) => {
-    const response = await context.next();
-    const page = await response.text();
-    const regex = 'Hello';
-    const msg = 'Hiii'
-    const updatedPage = page.replace(regex, msg);
+    const url = 'https://www.testhq.com/'
+    const response = await axios.get(url)
+    // const response2 = await context.next();
+    const page = await response.data.text();
+    const regex = 'Get Started';
+    const msg = 'Hello World'
+    const updatedPage = page.replaceAll(regex, msg);
 
     return new Response(updatedPage, response)
 }
--- netlify/edge-functions/transform-hello-template.js
+++
@@ -1,9 +0,0 @@
-export default async (request, context) => {
-  
-    const response = await context.next();
-    const page = await response.text();
-    const regex2 = /Hello/i;
-    const updatedPage = page.replace(regex2, 'hii');
-
-    return new Response(updatedPage, response)
-}
