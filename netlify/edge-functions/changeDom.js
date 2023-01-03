52aa0344b5583bc7165d16111d6a64d2665b7402
Adeee11 <hello@iwebcode.design>
2023-01-03 17:16:21 +0530

    testing

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,9 +1,36 @@
+import axios from "axios";
+
 export default async (request, context) => {
+    var config = {
+        method: 'get',
+        url: 'https://04d4-122-176-42-49.in.ngrok.io/modify',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Vary": "Access-Control-Request-Headers",
+            "Access-Control-Allow-Headers": "Content-Type,Accept"
+        }
+    };
     const response = await context.next();
     const page = await response.text();
-    const regex = 'Hello';
-    const msg = 'Hiii'
-    const updatedPage = page.replace(regex, msg);
 
-    return new Response(updatedPage, response)
+
+    axios(config)
+        .then(function (resp) {
+            // const updatedPage = page.replace(regex, msg);
+
+            return new Response(resp, response)
+
+            // console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    // const response = await context.next();
+    // const page = await response.text();
+    // const regex = 'Hello';
+    // const msg = 'Hiii'
+    // const updatedPage = page.replace(regex, msg);
+
+    // return new Response(updatedPage, response)
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,26 @@
+import axios from 'axios';
 import React, { useEffect } from 'react'
 
 const Home = () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Cookie", "__cf_bm=37qfEfn4CpMbhmwK7236vGMse2ESLMqXkM4kWTPiE4g-1672737564-0-Afp9WEPgDMFFgHpPLsL/ibHJuKGEXxQLv6c0clK+dXIHHvsXr51rglI21qLXa7glc7xkEBIdXgkSzqymLA9mPFE=; __cfruid=501746bb133d6ac0c18eb790f097f6afd7bd2755-1672737564");
+    var config = {
+        method: 'get',
+        url: 'https://04d4-122-176-42-49.in.ngrok.io/modify',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Vary": "Access-Control-Request-Headers",
+            "Access-Control-Allow-Headers": "Content-Type,Accept"
+        }
+    };
 
-    const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    } as any;
-    useEffect(() => {
-        fetch("https://www.testhq.com/", requestOptions)
-            .then(response => response)
-            .then(result => console.log('hello', result))
-            .catch(error => console.log('error', error));
-    }, [])
+    axios(config)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
 
     return (
         <div>
