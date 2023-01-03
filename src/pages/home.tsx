8ca57fd7cf58eb09289ffb3889252ed512f0c015
Adeee11 <hello@iwebcode.design>
2023-01-03 14:20:42 +0530

    test

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react'
 
 const Home = () => {
-    var requestOptions = {
-        method: 'GET',
-    };
+    // var requestOptions = {
+    //     method: 'GET',
+    // };
 
-    fetch("https://www.testhq.com/", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log('result', result))
-        .catch(error => console.log('error', error));
+    // fetch("https://www.testhq.com/", requestOptions)
+    //     .then(response => response.text())
+    //     .then(result => console.log('result', result))
+    //     .catch(error => console.log('error', error));
 
     return (
         <div>
