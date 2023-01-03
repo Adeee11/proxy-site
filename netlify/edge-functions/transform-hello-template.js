fcc62135e28f48f9adc713284e24d4f0903fc081
Adeee11 <hello@iwebcode.design>
2023-01-03 12:37:22 +0530

    changes

--- netlify/edge-functions/transform-hello-template.js
+++ netlify/edge-functions/transform-hello-template.js
@@ -1,18 +1,8 @@
 export default async (request, context) => {
-    // const url = new URL(request.url)
-
-    // if (url.searchParams.get("method") !== "transform") {
-    //     return;
-    // }
-
+  
     const response = await context.next();
     const page = await response.text();
-    const regex = /LOCATION_UNKNOWN/i;
     const regex2 = /Hello/i;
-
-    const location = `${context.geo.city} , ${context.geo.country.name}`
-    console.log('location', location);
-
     const updatedPage = page.replace(regex2, 'hii');
 
     return new Response(updatedPage, response)
