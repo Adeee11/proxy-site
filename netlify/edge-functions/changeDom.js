db64605332bcb5cef81fc9e47a0ae886d9fd8dd1
Adeee11 <hello@iwebcode.design>
2023-01-04 12:02:49 +0530

    urls

--- netlify/edge-functions/changeDom.js
+++ netlify/edge-functions/changeDom.js
@@ -1,8 +1,8 @@
 export default async (request, context) => {
     const response = await context.next();
     const page = await response.text();
-    const regex = 'Get Started';
-    const msg = 'Changed Content'
+    const regex = 'https://www.testhq.com/';
+    const msg = '/'
     const updatedPage = page.replaceAll(regex, msg);
 
     return new Response(updatedPage, response)
